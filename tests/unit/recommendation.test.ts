e6bf1776233e42e1230ca980f199e57979530c18
Caio <caiosobreira77@gmail.com>
2022-09-28 15:47:32 -0300

    feat:(unit) 100% coverage from services functions

--- tests/unit/recommendation.test.ts
+++ tests/unit/recommendation.test.ts
@@ -91,7 +91,7 @@ describe('Testing POST /recommendations/:id/downvote', () => {
     it('should delete a recommendation with score less than -5', async () => {
         const recommendation = recommendationFactory.createRecommendation()
         const EXPECTED_SCORE = -5
-        recommendation.score = -10
+        recommendation.score = -6
 
         jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation)
         jest.spyOn(recommendationRepository, 'updateScore').mockResolvedValueOnce(recommendation)
@@ -126,6 +126,79 @@ describe('Testing GET /recommendations/:id', () => {
 
         await expect(recommendationService.getById(recommendation.id)).rejects.toEqual(notFoundError())
         expect(recommendationRepository.find).toHaveBeenCalledWith(recommendation.id)
+    })
+})
+
+describe('Testing GET /recommendations', () => {
+    it('should get recommendation correctly', async () => {
+        const recommendation = recommendationFactory.tenRecommendations()
+
+        jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce(recommendation)
+
+        const findAll = await recommendationService.get()
+
+        expect(findAll).toBeInstanceOf(Array)
+        expect(findAll.length).toBe(10)
+        expect(findAll).toBe(recommendation)
+        expect(recommendationRepository.findAll).toHaveBeenCalled()
+     
+    })
+})
+
+describe('Testing GET /recommendations/top/:amount', () => {
+    it('should get many recommendation ordered by score', async () => {
+        const recommendations = recommendationFactory.orderedTenRecommendations()
+        const amount = 10
+
+        jest.spyOn(recommendationRepository, 'getAmountByScore').mockResolvedValueOnce(recommendations)
+
+        const topRecommendations = await recommendationService.getTop(amount)
+
+        expect(topRecommendations).toBeInstanceOf(Array)
+        expect(topRecommendations.length).toBe(amount)
+        expect(topRecommendations).toBe(recommendations)
+        expect(recommendationRepository.getAmountByScore).toHaveBeenCalledWith(amount)
+     
+    })
+})
+
+
+describe('Testing GET /recommendations/random', () => {
+    it('Should return 200 given a random recommendation with score less than or equal 10', async () => {
+        const recommendations = recommendationFactory.tenRecommendations()
+
+        jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce(recommendations)
+        jest.spyOn(Math, 'random').mockReturnValue(0.7)
+
+        const randomRecommendation = await recommendationService.getRandom()
+
+        expect(randomRecommendation).toBeInstanceOf(Object)
+        expect(recommendations).toContain(randomRecommendation)
+        expect(Math.random).toHaveBeenCalled()
+        expect(recommendationRepository.findAll).toHaveBeenCalled()
+    })
+
+    it('Should return 200 given a random recommendation with score greater than or equal', async () => {
+        const recommendations = recommendationFactory.tenRecommendations()
+
+        jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce(recommendations)
+        jest.spyOn(Math, 'random').mockReturnValue(0.6)
+
+        const randomRecommendation = await recommendationService.getRandom()
+
+        expect(randomRecommendation).toBeInstanceOf(Object)
+        expect(recommendations).toContain(randomRecommendation)
+        expect(Math.random).toHaveBeenCalled()
+        expect(recommendationRepository.findAll).toHaveBeenCalled()
+    })
+
+    it('should return not found (404) if not found any recommendation', async () => {
+        jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce([])
+        jest.spyOn(recommendationRepository, 'findAll').mockResolvedValueOnce([])
+
+        await expect(recommendationService.getRandom).rejects.toEqual(notFoundError())
+
+        expect(recommendationRepository.findAll).toHaveBeenCalled()
     })
 })
 
